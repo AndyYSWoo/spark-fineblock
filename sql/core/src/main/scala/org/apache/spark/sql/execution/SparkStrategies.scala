585646d95732d49c81c7c128a117ac0af687fe1b
Yongshang WU@IIECAS <andyyswoo@gmail.com>
2016-09-26 10:51:34 +0800

    figure out ParquetTableScan(_, p2, p3) a partially applied function.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
@@ -256,9 +256,9 @@ private[sql] abstract class SparkStrategies extends QueryPlanner[SparkPlan] {
           projectList,
           filters,
           prunePushedDownFilters,
-          ParquetTableScan(
-            _,
-            relation,
+          ParquetTableScan( // This param of pruneFilterProject is a function of type Seq[Attribute] => SparkPlan,
+            _,              // While ParquetTableScan is a case class which takes params of Seq[Attribute], ParquetRelation, Seq[Expression].
+            relation,       // Using _ as the first param, i.e. ParquetTableScan(_, p2, p3), makes this a partially applied function
             if (sqlContext.conf.parquetFilterPushDown) filtersToPush else Nil)) :: Nil
 
       case _ => Nil
